623762ef08af066541afa724e8545124061ae13d
Jakob <72307968+jakob30061@users.noreply.github.com>
2024-04-21 11:17:41 +0200

    it started to work again

---
+++ src/onsend/onsend.ts
@@ -0,0 +1,98 @@
+/* eslint-disable no-undef */
+/* eslint-disable prettier/prettier */
+/* eslint-disable office-addins/no-office-initialize */
+
+let mailboxItem: Office.MessageCompose;
+
+Office.initialize = function (reason) {
+    mailboxItem = Office.context.mailbox.item;
+}
+
+function getGlobal() {
+    return typeof self !== "undefined"
+        ? self
+        : typeof window !== "undefined"
+            ? window
+            : typeof global !== "undefined"
+                ? global
+                : undefined;
+}
+
+export const g = getGlobal() as any;
+
+// The ui-less functions need to be available in global scope
+g.validateEmailAddresses = validateEmailAddresses;
+
+let addressesDialog: Office.Dialog;
+function validateEmailAddresses(event) {
+    mailboxItem.body.getAsync("text", { asyncContext: event }, result => {
+        let showNotification = false
+        if (result.status === Office.AsyncResultStatus.Succeeded) {
+            showNotification = result.value.length < 30
+
+            const strings = {
+              attachments: 'Die Email scheint einen großen Anhang zu haben',
+              insufficient_content: 'Die Email scheint über wenig Inhalt verfügen',
+              many_recipient: 'Die Email hat viele Empfänger.'
+            }
+
+            return mailboxItem.to.getAsync((asyncResult) => {
+                if (asyncResult.status === Office.AsyncResultStatus.Failed)
+                  return showNotification = showNotification ? showNotification : false;
+                
+                showNotification = showNotification ? showNotification : asyncResult.value.length < 30
+
+                if(showNotification) {
+                    let url: string = `${window.location.origin}/dialog.html`;
+    
+                    Office.context.ui.displayDialogAsync(url, getDialogOptions(), dialogResult => {
+                        addressesDialog = dialogResult.value;
+                        addressesDialog.addEventHandler(Office.EventType.DialogMessageReceived, (message) => receiveMessage(message, event));
+                        addressesDialog.addEventHandler(Office.EventType.DialogEventReceived, () => dialogClosed(event));
+                    });
+                }
+            });
+        }
+    });
+    event.completed({ allowEvent: true });
+}
+
+function getDialogOptions(): Office.DialogOptions {
+    let dialogOptions: Office.DialogOptions;
+    // height: height of the dialog as a percentage of the current display. Defaults to 80%. 250px minimum
+    // width: width of the dialog as a percentage of the current display. Defaults to 80%. 150px minimum
+
+    if (Office.context.diagnostics.platform === Office.PlatformType.OfficeOnline) { // Browser
+
+        // On Browser the iframe cannot get the dimensions of the parent window since it is on another domain
+        dialogOptions = { width: 35, height: 9, displayInIframe: true }; // Browser
+
+    } else { // Desktop
+        let fixedWidth = 550;
+        let fixedHeight = 160;
+        let percentageWidth: number;
+        let percentageHeight: number;
+
+        percentageWidth = Math.round(100 * fixedWidth / screen.width);
+        percentageHeight = Math.round(100 * fixedHeight / screen.height);
+
+        dialogOptions = { width: percentageWidth, height: percentageHeight, displayInIframe: false }; // Desktop
+    }
+    return dialogOptions;
+}
+
+function receiveMessage(message: any, event: any) {
+    addressesDialog.close();
+    addressesDialog = null;
+
+    if (message.message === "Send") {
+        event.completed({ allowEvent: true });
+    } else {
+        event.completed({ allowEvent: false });
+    }
+}
+
+function dialogClosed(event: any) {
+    addressesDialog = null;
+    event.completed({ allowEvent: false });
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,94 +1,113 @@
 /* eslint-disable no-undef */
 
-const devCerts = require("office-addin-dev-certs");
-const CopyWebpackPlugin = require("copy-webpack-plugin");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const devCerts = require( "office-addin-dev-certs" );
+const CopyWebpackPlugin = require( "copy-webpack-plugin" );
+const HtmlWebpackPlugin = require( "html-webpack-plugin" );
 
 const urlDev = "https://localhost:3000/";
-const urlProd = ""; // CHANGE THIS TO YOUR PRODUCTION DEPLOYMENT LOCATION
+const urlProd = "";
 
 async function getHttpsOptions() {
-  const httpsOptions = await devCerts.getHttpsServerOptions();
-  return { ca: httpsOptions.ca, key: httpsOptions.key, cert: httpsOptions.cert };
+    const httpsOptions = await devCerts.getHttpsServerOptions();
+    return { cacert: httpsOptions.ca, key: httpsOptions.key, cert: httpsOptions.cert };
 }
 
-module.exports = async (env, options) => {
-  const dev = options.mode === "development";
-  const config = {
-    devtool: "source-map",
-    entry: {
-      polyfill: ["core-js/stable", "regenerator-runtime/runtime"],
-      interceptor: "./src/interceptor.ts",
-    },
-    output: {
-      clean: true,
-    },
-    resolve: {
-      extensions: [".ts", ".html", ".js"],
-    },
-    module: {
-      rules: [
-        {
-          test: /\.ts$/,
-          exclude: /node_modules/,
-          use: {
-            loader: "babel-loader",
-            options: {
-              presets: ["@babel/preset-typescript"],
-            },
-          },
+module.exports = async ( env, options ) => {
+    const dev = options.mode === "development";
+    const config = {
+        devtool: "source-map",
+        optimization: {
+            // The default minified version in production mode does not work
+            minimize: false // force to not minify
         },
-        {
-          test: /\.html$/,
-          exclude: /node_modules/,
-          use: "html-loader",
+        entry: {
+            polyfill: [ "core-js/stable", "regenerator-runtime/runtime" ],
+            onsend: "./src/onsend/onsend.ts",
+            dialog: "./src/dialog/dialog.ts",
         },
-        {
-          test: /\.(png|jpg|jpeg|gif|ico)$/,
-          type: "asset/resource",
-          generator: {
-            filename: "assets/[name][ext][query]",
-          },
+        output: {
+            devtoolModuleFilenameTemplate: "webpack:///[resource-path]?[loaders]",
+            clean: true,
         },
-      ],
-    },
-    plugins: [
-      new CopyWebpackPlugin({
-        patterns: [
-          {
-            from: "assets/*",
-            to: "assets/[name][ext][query]",
-          },
-          {
-            from: "manifest*.xml",
-            to: "[name]" + "[ext]",
-            transform(content) {
-              if (dev) {
-                return content;
-              } else {
-                return content.toString().replace(new RegExp(urlDev, "g"), urlProd);
-              }
-            },
-          },
-        ],
-      }),
-      new HtmlWebpackPlugin({
-        filename: "index.html",
-        template: "./src/index.html",
-        chunks: ["polyfill", "interceptor"],
-      }),
-    ],
-    devServer: {
-      headers: {
-        "Access-Control-Allow-Origin": "*",
-      },
-      server: {
-        type: "https",
-        options: env.WEBPACK_BUILD || options.https !== undefined ? options.https : await getHttpsOptions(),
-      },
-      port: process.env.npm_package_config_dev_server_port || 3000,
-    },
-  };
+        resolve: {
+            extensions: [ ".ts", ".tsx", ".html", ".js" ],
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.ts$/,
+                    exclude: /node_modules/,
+                    use: {
+                        loader: "babel-loader",
+                        options: {
+                            presets: [ "@babel/preset-typescript" ],
+                        },
+                    },
+                },
+                {
+                    test: /\.tsx?$/,
+                    exclude: /node_modules/,
+                    use: "ts-loader",
+                },
+                {
+                    test: /\.html$/,
+                    exclude: /node_modules/,
+                    use: "html-loader",
+                },
+                // {
+                //     test: /\.(png|jpg|jpeg|gif|ico)$/,
+                //     type: "asset/resource",
+                //     generator: {
+                //         filename: "assets/[name][ext][query]",
+                //     },
+                // },
+            ],
+        },
+        plugins: [
+            new HtmlWebpackPlugin( {
+                filename: "onsend.html",
+                template: "./src/onsend/onsend.html",
+                chunks: [ "polyfill", "onsend" ],
+            } ),
+            new CopyWebpackPlugin( {
+                patterns: [
+                    // {
+                    //     from: "assets/*",
+                    //     to: "assets/[name][ext][query]",
+                    // },
+                    {
+                        from: "manifest*.xml",
+                        to: "[name]" + "[ext]",
+                        transform( content ) {
+                            return content.toString().replace( new RegExp( urlDev, "g" ), urlProd );
 
-  return config;
+                            if ( dev ) {
+                                return content;
+                            } else {
+                                return content.toString().replace( new RegExp( urlDev, "g" ), urlProd );
+                            }
+                        },
+                    },
+                    {
+                        from: "./src/dialog/dialog.css",
+                        to: "dialog.css",
+                    },
+                ],
+            } ),
+            new HtmlWebpackPlugin( {
+                filename: "dialog.html",
+                template: "./src/dialog/dialog.html",
+                chunks: [ "polyfill", "dialog" ],
+            } ),
+        ],
+        devServer: {
+            headers: {
+                "Access-Control-Allow-Origin": "*",
+            },
+            https: env.WEBPACK_BUILD || options.https !== undefined ? options.https : await getHttpsOptions(),
+            port: process.env.npm_package_config_dev_server_port || 3000,
+        },
+    };
+
+    return config;
 };
